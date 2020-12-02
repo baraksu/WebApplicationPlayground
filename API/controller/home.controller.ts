6e75034d475ca7d573d7f418f1fab5cbe43da97e
Barak Suberri <BSuberri@optimaltest.com>
2020-12-02 17:32:25 +0200

    Set API dir for backend

--- API/controller/home.controller.ts
+++ API/controller/home.controller.ts
@@ -2,6 +2,6 @@ import { NextFunction, Request, Response } from 'express';
 
 export default class HomeController {
   public static getDefault(req: Request, res: Response, next: NextFunction): void {
-    res.send('Hello World! 200');
+    res.send('Hello World! 202');
   }
 }
